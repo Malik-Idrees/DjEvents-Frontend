1db5970f0f5881f9cb105fe1186b13b9c6ee22b6
Malik-Idrees <{ID}+{username}@users.noreply.github.com>
2021-09-24 00:53:55 +0500

    event details page

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,8 +1,93 @@
+import { FaPencilAlt, FaTimes } from 'react-icons/fa'
+import Link from 'next/link'
+import Image from 'next/image'
 import Layout from '@/components/Layout'
-export default function EventPage() {
+import { API_URL } from '@/config/index'
+import styles from '@/styles/Event.module.css'
+
+export default function EventPage({ evt }) {
+   const deleteEvent = (e) => {
+      console.log('delete')
+   }
+
    return (
       <Layout>
-         <h1>My Event</h1>
+         <div className={styles.event}>
+            <div className={styles.controls}>
+               <Link href={`/events/edit/${evt.id}`}>
+                  <a>
+                     <FaPencilAlt /> Edit Event
+                  </a>
+               </Link>
+               <a href='#' className={styles.delete} onClick={deleteEvent}>
+                  <FaTimes /> Delete Event
+               </a>
+            </div>
+            <span>
+               {evt.date} at {evt.time}
+            </span>
+            <h1>{evt.name}</h1>
+            {evt.image && (
+               <div className={styles.image}>
+                  <Image src={evt.image} width={960} height={600} />
+               </div>
+            )}
+
+            <h3>Performers:</h3>
+            <p>{evt.performers}</p>
+            <h3>Description:</h3>
+            <p>{evt.description}</p>
+            <h3>Venue: {evt.venue}</h3>
+            <p>{evt.address}</p>
+
+            <Link href='/events'>
+               <a className={styles.back}>{'<'}Go Back</a>
+            </Link>
+         </div>
       </Layout>
    )
 }
+
+export async function getStaticPaths() {
+   const res = await fetch(`${API_URL}/api/events`)
+   const evevts = await res.json()
+
+   const paths = evevts.map((evt) => ({
+      params: { slug: evt.slug },
+   }))
+
+   return {
+      paths,
+      fallback: true, //If false: Show 404 if the slug is not found
+   }
+   // return {
+   //    paths: [
+   //       {params: {id/slug:1}},
+   //       {params: {id:2}},
+   //       {params: {id:3}},
+   //    ]
+   // }
+}
+
+export async function getStaticProps({ params: { slug } }) {
+   const res = await fetch(`${API_URL}/api/events/${slug}`)
+   const event = await res.json()
+   return {
+      props: {
+         evt: event[0],
+      },
+      revalidate: 1,
+   }
+}
+
+// export async function getServerSideProps({ query: { slug } }) {
+// props.query.slug
+//    const res = await fetch(`${API_URL}/api/events/${slug}`)
+//    const event = await res.json()
+//    console.log(event)
+//    return {
+//       props: {
+//          evt: event[0],
+//       },
+//    }
+// }
